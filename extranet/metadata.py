cebbd8e72da59f61a3205e76dedf0e3de1bf1320
Cédric Picard <mystiro@gmail.com>
2014-11-26 03:08:07 +0100

    Moved extranet's library away from the repo
    I also generalised it, you now need to add the url of the extranet.

--- extranet/metadata.py
+++
@@ -1,7 +0,0 @@
-__version__ = '0.1'
-__author__ = 'Benoît Taine'
-__copyright__ = 'Copyright 2014, Benoît Taine'
-__maintainer__ = 'Benoît Taine'
-__email__ = 'benoit.taine@efrei.net'
-__license__ = 'GNU LGPL v3'
-
